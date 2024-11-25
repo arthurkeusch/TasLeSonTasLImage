17bc32b52fd572a6a9d501b32ec4a70bff29a4d3
arthurkeusch <113102000+arthurkeusch@users.noreply.github.com>
2024-11-25 16:52:59 +0100

    Création de la classe Image

--- TasLeSonTasLImage/src/main/java/arthurkeusch/taslesontaslimage/Image.java
+++ TasLeSonTasLImage/src/main/java/arthurkeusch/taslesontaslimage/Image.java
@@ -1,8 +1,15 @@
 package arthurkeusch.taslesontaslimage;
 
+import java.util.ArrayList;
+
 public class Image {
+    public ArrayList<Integer> image;
 
-    // matrice qui represente les frequence de chaque pixel
+    public ArrayList<Integer> getImage() {
+        return image;
+    }
 
-
+    public void setImage(ArrayList<Integer> image) {
+        this.image = image;
+    }
 }
