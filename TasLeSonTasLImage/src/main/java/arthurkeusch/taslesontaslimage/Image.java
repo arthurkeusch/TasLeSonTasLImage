dd9100a1f8fba3c25fc63f41ba206113277fdeda
arthurkeusch <113102000+arthurkeusch@users.noreply.github.com>
2024-11-25 17:10:52 +0100

    Mise à jour de la classe Image

--- TasLeSonTasLImage/src/main/java/arthurkeusch/taslesontaslimage/Image.java
+++ TasLeSonTasLImage/src/main/java/arthurkeusch/taslesontaslimage/Image.java
@@ -3,13 +3,31 @@ package arthurkeusch.taslesontaslimage;
 import java.util.ArrayList;
 
 public class Image {
-    public ArrayList<Integer> image;
+    private ArrayList<ArrayList<Integer>> image;
 
-    public ArrayList<Integer> getImage() {
+    public Image(ArrayList<ArrayList<Integer>> image) {
+        this.image = image;
+    }
+
+    public ArrayList<ArrayList<Integer>> getImage() {
         return image;
     }
 
-    public void setImage(ArrayList<Integer> image) {
+    public void setImage(ArrayList<ArrayList<Integer>> image) {
         this.image = image;
+    }
+
+    public Integer getPixel(int row, int col) {
+        return this.image.get(row).get(col);
+    }
+
+    public void setPixel(int row, int col, Integer pixel) {
+        this.image.get(row).set(col, pixel);
+    }
+
+    public void printImage() {
+        for (ArrayList<Integer> row : this.image) {
+            System.out.println(row);
+        }
     }
 }
--- TasLeSonTasLImage/src/main/java/arthurkeusch/taslesontaslimage/TraitementImage.java
+++ TasLeSonTasLImage/src/main/java/arthurkeusch/taslesontaslimage/TraitementImage.java
@@ -4,7 +4,21 @@ public class TraitementImage {
 
     // niveau de gris
 
-    // format image 64x64
+    /***
+     * Change le ratio de l'iamge pour la mettre en 64x64
+     * @param imageLink → Le lien de l'image
+     */
+    public void changeImageRatio(String imageLink) {
+
+    }
 
     // fonction qui prend une image et renvoie une matrice de frequence
+
+    /**
+     *
+     * @param image
+     */
+    public void generateImageSound(Image image) {
+
+    }
 }
