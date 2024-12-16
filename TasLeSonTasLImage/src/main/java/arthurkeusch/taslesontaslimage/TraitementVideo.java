5d82dff65ca1b63b7d6a261db89f763f228540d9
arthurkeusch <113102000+arthurkeusch@users.noreply.github.com>
2024-12-16 15:43:02 +0100

    Optimisation temps traitement vidéo

--- TasLeSonTasLImage/src/main/java/arthurkeusch/taslesontaslimage/TraitementVideo.java
+++ TasLeSonTasLImage/src/main/java/arthurkeusch/taslesontaslimage/TraitementVideo.java
@@ -1,16 +1,18 @@
 package arthurkeusch.taslesontaslimage;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Classe permettant de traiter une vidéo et d'extraire des images à intervalles réguliers.
@@ -18,64 +20,35 @@ import org.opencv.videoio.Videoio;
 public class TraitementVideo {
 
     /**
-     * Extrait une image toutes les secondes d'une vidéo et les enregistre dans un dossier spécifique.
-     * Le dossier est vidé avant tout traitement.
+     * Traite un segment de la vidéo entre deux secondes spécifiques.
      *
-     * @param videoPath Le chemin de la vidéo à traiter.
+     * @param videoPath   Chemin de la vidéo à traiter.
+     * @param startSecond Seconde de début du segment.
+     * @param endSecond   Seconde de fin du segment.
      */
-    public void traitementVideo(String videoPath) {
-        // Charger la bibliothèque native OpenCV
+    public void traiterSegment(String videoPath, int startSecond, int endSecond) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-        // Chemin du dossier de sortie
-        String outputFolder = "src/main/imagesVideo";
-
-        // Vider le dossier de sortie
-        File directory = new File(outputFolder);
-        if (directory.exists()) {
-            for (File file : directory.listFiles()) {
-                file.delete();
-            }
-        } else {
-            directory.mkdirs();
-        }
-
-        // Ouvrir la vidéo
         VideoCapture videoCapture = new VideoCapture(videoPath);
+
         if (!videoCapture.isOpened()) {
             System.out.println("Impossible d'ouvrir la vidéo : " + videoPath);
             return;
         }
 
-        // Récupérer les informations de la vidéo
-        double fps = videoCapture.get(Videoio.CAP_PROP_FPS);
-        double frameIntervalMs = 1000; // Intervalle en millisecondes pour une seconde
-        double currentTimeMs = 0; // Timestamp en millisecondes
-
         Mat frame = new Mat();
-        int imageIndex = 0;
+        for (int seconde = startSecond; seconde < endSecond; seconde++) {
+            videoCapture.set(Videoio.CAP_PROP_POS_MSEC, seconde * 1000);
 
-        while (true) {
-            videoCapture.set(Videoio.CAP_PROP_POS_MSEC, currentTimeMs);
-            if (!videoCapture.read(frame) || frame.empty()) {
-                break; // Fin de la vidéo
-            }
-
-            // Convertir la frame en BufferedImage
-            BufferedImage image = matToBufferedImage(frame);
-            if (image != null) {
-                // Enregistrer l'image
-                File outputFile = new File(outputFolder, "image_" + imageIndex + ".png");
-                try {
-                    ImageIO.write(image, "png", outputFile);
-                    System.out.println("Image enregistrée : " + outputFile.getAbsolutePath());
-                    imageIndex++;
-                } catch (IOException e) {
-                    e.printStackTrace();
+            if (videoCapture.read(frame) && !frame.empty()) {
+                BufferedImage image = matToBufferedImage(frame);
+                if (image != null) {
+                    String outputPath = "src/main/imagesVideo/image_" + seconde + ".jpg";
+                    saveImageWithCompression(image, outputPath);
+                    System.out.println("Image extraite à la seconde " + seconde + " : " + outputPath);
                 }
+            } else {
+                System.out.println("Aucune image trouvée à la seconde " + seconde);
             }
-
-            currentTimeMs += frameIntervalMs; // Passer au prochain intervalle d'une seconde
         }
 
         videoCapture.release();
@@ -100,5 +73,52 @@ public class TraitementVideo {
         System.arraycopy(sourcePixels, 0, targetPixels, 0, sourcePixels.length);
 
         return image;
+    }
+
+    /**
+     * Enregistre une image en format JPEG avec compression.
+     *
+     * @param image      L'image à sauvegarder.
+     * @param outputPath Le chemin du fichier de sortie.
+     */
+    private void saveImageWithCompression(BufferedImage image, String outputPath) {
+        try {
+            ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
+            ImageWriteParam param = writer.getDefaultWriteParam();
+            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+            param.setCompressionQuality(0.8f); // Compression à 80 % de qualité
+
+            File outputFile = new File(outputPath);
+            try (ImageOutputStream ios = ImageIO.createImageOutputStream(outputFile)) {
+                writer.setOutput(ios);
+                writer.write(null, new IIOImage(image, null, null), param);
+            }
+            writer.dispose();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Obtenir la durée de la vidéo en secondes.
+     *
+     * @param videoPath Chemin de la vidéo.
+     * @return Durée de la vidéo en secondes.
+     */
+    public int obtenirDureeVideo(String videoPath) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        VideoCapture videoCapture = new VideoCapture(videoPath);
+
+        if (!videoCapture.isOpened()) {
+            System.out.println("Impossible d'ouvrir la vidéo : " + videoPath);
+            return 0;
+        }
+
+        double frameCount = videoCapture.get(Videoio.CAP_PROP_FRAME_COUNT);
+        double fps = videoCapture.get(Videoio.CAP_PROP_FPS);
+
+        videoCapture.release();
+
+        return (int) Math.ceil(frameCount / fps);
     }
 }
